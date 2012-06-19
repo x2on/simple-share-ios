79972b5c13c873b17f81e7b5310fd30e7dd4b26a
Mark Rickert <mjar81@gmail.com>
2012-06-19 17:26:59 -0400

    Added iOS App ID, app description, and app icon URL to the configuration.

--- simple-share/SimpleFacebookConfiguration.m
+++ simple-share/SimpleFacebookConfiguration.m
@@ -18,10 +18,47 @@
 //  limitations under the License.
 
 #import "SimpleFacebookConfiguration.h"
+#import "JSONKit.h"
 
 @implementation SimpleFacebookConfiguration
 @synthesize appId;
+@synthesize iOSAppId;
 @synthesize appName;
+@synthesize appDescription;
 @synthesize appUrl;
+@synthesize appIconUrl;
+
+-(NSString *) getAppDescription
+{
+    if(self.description == nil)return @"";
+    else return self.description;
+}
+
+-(void) setIOSAppId:(NSString *)aID
+{
+    NSLog(@"Setting is app id : %@", aID);
+    
+    self->iOSAppId = aID;
+    
+    //Grab the photo url as well.
+    NSURL *appInfoURL = [NSURL URLWithString:[NSString stringWithFormat:@"http://ax.phobos.apple.com.edgesuite.net/WebObjects/MZStoreServices.woa/wa/wsLookup?id=%@", self.iOSAppId]];
+    NSURLRequest *appInfoRequest = [NSURLRequest requestWithURL:appInfoURL];
+    
+    NSURLResponse *resp = nil;
+    NSError *err = nil;
+    NSData *response = [NSURLConnection sendSynchronousRequest:appInfoRequest returningResponse:&resp error:&err];
+    NSString * theString = [[NSString alloc] initWithData:response encoding:NSUTF8StringEncoding]; 
+    
+    NSDictionary *appInfo = [theString objectFromJSONString];
+    NSDictionary *appInfoResults = [[appInfo objectForKey:@"results"] objectAtIndex:0];
+    
+    self.appIconUrl = [appInfoResults objectForKey:@"artworkUrl512"];    
+}
+
+-(NSString *) getAppIconUrl
+{
+    if(self.appIconUrl != nil)return self.appIconUrl;   
+    else return @"";
+}
 
 @end
